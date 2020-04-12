75816063378a9086b63010d14f52e7ee5421e39e
nerisina <neri.esparza@gmail.com>
2020-04-12 18:22:12 -0400

    env port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Datastore = require('nedb')
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const fetch = require('node-fetch'); 
 require('dotenv').config()
